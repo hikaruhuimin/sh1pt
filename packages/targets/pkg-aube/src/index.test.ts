d19cb8826fe00f1cbfe2c9d1eaa323bc66411699
hikaruhuimin <hikaru0701@qq.com>
2026-05-10 19:51:23 +0900

    feat: add Stripe payment adapter (squashed)

--- packages/targets/pkg-aube/src/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { contractTestTarget } from '@profullstack/sh1pt-core/testing';
-import target from './index.js';
-
-contractTestTarget(target, {
-  sampleConfig: { packageDir: './fake', packageName: '@acme/fake', access: 'public' },
-});
